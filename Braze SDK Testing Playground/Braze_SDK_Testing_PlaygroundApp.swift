ac74c6abeb3eadf8762b4d6325c61ff5fd7edd80
ryand913 <98771607+ryand913@users.noreply.github.com>
2024-01-09 16:21:55 -0500

    Initial Commit

---
+++ Braze SDK Testing Playground/Braze_SDK_Testing_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Braze_SDK_Testing_PlaygroundApp.swift
+//  Braze SDK Testing Playground
+//
+//  Created by Ryan Dusenbury on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Braze_SDK_Testing_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Braze SDK Testing Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Braze SDK Testing Playground
+//
+//  Created by Ryan Dusenbury on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
