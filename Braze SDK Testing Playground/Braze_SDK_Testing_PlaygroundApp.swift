8a2453cebf6bf2d53c6fde004fbbafbe30a60685
ryand913 <98771607+ryand913@users.noreply.github.com>
2024-01-09 19:45:34 -0500

    Adding project

--- Braze SDK Testing Playground/Braze_SDK_Testing_PlaygroundApp.swift
+++ Braze SDK Testing Playground/Braze_SDK_Testing_PlaygroundApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import BrazeKit
+import BrazeUI
 
 @main
 struct Braze_SDK_Testing_PlaygroundApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+static var braze: Braze? = nil
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+      
+      let configuration = Braze.Configuration(
+            apiKey: "0bc2f7b0-6de2-4c29-8050-4914d4f4b59e",
+            endpoint: "sondheim.braze.com"
+      )
+      let braze = Braze(configuration: configuration)
+      AppDelegate.braze = braze
+    return true
+  }
+
+}
+
